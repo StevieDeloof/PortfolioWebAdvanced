644a8b99da01aa76fb944d3546a4b2c4fa0f198e
StevieDeloof <145539261+StevieDeloof@users.noreply.github.com>
2025-05-02 10:50:46 +0200

    Made the ability to filter the amount of pokemon you get

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 'use strict'
 import { getImage } from "./searchFunctions.js"
 const poketable = document.getElementById("poketable")
+const amountEl = document.getElementById('amount')
 
 const searchPokemon = async (amount = 10) => {
+    poketable.innerHTML = ""
+    console.log(amount)
     let i = 1;
     while (amount > 0) {
         const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`).then(data => data.json())
@@ -17,7 +20,9 @@ const searchPokemon = async (amount = 10) => {
         newPokemon.appendChild(data);
         poketable.appendChild(newPokemon)
         i++
+        amount--;
     }
 }
 
 document.addEventListener('load', searchPokemon())
+document.getElementById("filter").addEventListener("click", () => {searchPokemon(parseFloat(amountEl.value))})
